0b908ab199f0ca53a4222d5e8e3219ca6eba5dee
Maheshkumar N <maheshkumarn@live.com>
2020-01-30 10:46:30 +0000

    Contact CosmosDb

--- Models/Concrete/CosmosContactRepository.cs
+++ Models/Concrete/CosmosContactRepository.cs
@@ -10,66 +10,161 @@ using System.Net;
 
 namespace ContactsCoreMVC.Models.Concrete
 {
-  public class CosmosContactRepository : IContactRepository
-  {
-    private readonly string _cosmosEndpoint;
-    private readonly string _cosmosKey;
-    private readonly string _databaseId;
-    private readonly string _containerId;
-    private Database _database;
-    private Container _container;
-    private CosmosClient _cosmosClient;
-
-    public CosmosContactRepository(IOptions<CosmosUtility> cosmosUtility)
+    public class CosmosContactRepository : IContactRepository
     {
-      _cosmosEndpoint = cosmosUtility.Value.CosmosEndpoint;
-      _cosmosKey = cosmosUtility.Value.CosmosKey;
-      _databaseId = "multiDb";
-      _containerId = "contact";
+        private readonly string _cosmosEndpoint;
+        private readonly string _cosmosKey;
+        private readonly string _databaseId;
+        private readonly string _containerId;
+        private Database _database;
+        private Container _container;
+        private CosmosClient _cosmosClient;
 
-      _cosmosClient = new CosmosClient(_cosmosEndpoint, _cosmosKey);
-      _cosmosClient.CreateDatabaseIfNotExistsAsync(_databaseId).GetAwaiter().GetResult();
-      _database = _cosmosClient.GetDatabase(_databaseId);
-      _database.CreateContainerIfNotExistsAsync(_containerId, "/ContactName").GetAwaiter().GetResult();
-      _container = _database.GetContainer(_containerId);
-    }
+        public CosmosContactRepository(IOptions<CosmosUtility> cosmosUtility)
+        {
+            _cosmosEndpoint = cosmosUtility.Value.CosmosEndpoint;
+            _cosmosKey = cosmosUtility.Value.CosmosKey;
+            _databaseId = "multiDb";
+            _containerId = "contact";
 
-    public async Task<ContactTable> CreateAsync(ContactTable contactTable)
-    {
-      return null;
-    }
+            _cosmosClient = new CosmosClient(_cosmosEndpoint, _cosmosKey);
+            _cosmosClient.CreateDatabaseIfNotExistsAsync(_databaseId).GetAwaiter().GetResult();
+            _database = _cosmosClient.GetDatabase(_databaseId);
+            _database.CreateContainerIfNotExistsAsync(_containerId, "/ContactName").GetAwaiter().GetResult();
+            _container = _database.GetContainer(_containerId);
+        }
 
-    public async Task<ContactTable> FindContactAsync(string id)
-    {
-      return null;
-    }
-    public async Task<List<ContactTable>> FindContactCPAsync(string contactName, string phone)
-    {
-      return null;
-    }
+        public async Task<ContactTable> CreateAsync(ContactTable contactTable)
+        {
+            ItemResponse<ContactTable> contactResponse = await _container.CreateItemAsync<ContactTable>(contactTable);
+            if (contactResponse.StatusCode == HttpStatusCode.Created)
+            {
+                return contactTable;
+            }
+            return null;
+        }
 
-    public async Task<List<ContactTable>> FindContactByPhoneAsync(string phone)
-    {
-      return null;
-    }
+        public async Task<ContactTable> FindContactAsync(string id)
+        {
+            var sqlQuery = $"Select * from c where c.id='{id}'";
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQuery);
+            FeedIterator<ContactTable> queryResultSetIterator = _container.GetItemQueryIterator<ContactTable>(queryDefinition);
 
-    public async Task<List<ContactTable>> FindContactsByContactNameAsync(string contactName)
-    {
-      return null;
-    }
+            List<ContactTable> contactTableList = new List<ContactTable>();
 
-    public async Task<List<ContactTable>> GetAllContactsAsync()
-    {
-      return null;
-    }
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<ContactTable> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (var item in currentResultSet)
+                {
+                    contactTableList.Add(item);
+                }
+                return contactTableList[0];
+            }
+            return null;
+        }
+        public async Task<List<ContactTable>> FindContactCPAsync(string contactName, string phone)
+        {
+            var sqlQuery = $"Select * from c where c.ContactName='{contactName}' and c.Phone='{phone}'";
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQuery);
+            FeedIterator<ContactTable> queryResultSetIterator = _container.GetItemQueryIterator<ContactTable>(queryDefinition);
 
-    public async Task<ContactTable> UpdateAsync(ContactTable contactTable)
-    {
-      return null;
-    }
+            List<ContactTable> contactTableList = new List<ContactTable>();
 
-    public async Task DeleteAsync(string id, string contactName)
-    {
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<ContactTable> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (var item in currentResultSet)
+                {
+                    contactTableList.Add(item);
+                }
+                return contactTableList;
+            }
+            return null;
+        }
+
+        public async Task<List<ContactTable>> FindContactByPhoneAsync(string phone)
+        {
+            var sqlQuery = $"Select * from c where c.Phone='{phone}'";
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQuery);
+            FeedIterator<ContactTable> queryResultSetIterator = _container.GetItemQueryIterator<ContactTable>(queryDefinition);
+
+            List<ContactTable> contactTableList = new List<ContactTable>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<ContactTable> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (var item in currentResultSet)
+                {
+                    contactTableList.Add(item);
+                }
+                return contactTableList;
+            }
+            return null;
+        }
+
+        public async Task<List<ContactTable>> FindContactsByContactNameAsync(string contactName)
+        {
+            var sqlQuery = $"Select * from c where c.ContactName='{contactName}'";
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQuery);
+            FeedIterator<ContactTable> queryResultSetIterator = _container.GetItemQueryIterator<ContactTable>(queryDefinition);
+
+            List<ContactTable> contactTableList = new List<ContactTable>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<ContactTable> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (var item in currentResultSet)
+                {
+                    contactTableList.Add(item);
+                }
+                return contactTableList;
+            }
+            return null;
+        }
+
+        public async Task<List<ContactTable>> GetAllContactsAsync()
+        {
+            var sqlQuery = $"Select * from c";
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQuery);
+            FeedIterator<ContactTable> queryResultSetIterator = _container.GetItemQueryIterator<ContactTable>(queryDefinition);
+
+            List<ContactTable> contactTableList = new List<ContactTable>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<ContactTable> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (var item in currentResultSet)
+                {
+                    contactTableList.Add(item);
+                }
+                return contactTableList;
+            }
+            return null;
+        }
+
+        public async Task<ContactTable> UpdateAsync(ContactTable contactTable)
+        {
+            ItemResponse<ContactTable> contactResponse = await _container.ReadItemAsync<ContactTable>(contactTable.Id, new PartitionKey(contactTable.ContactName));
+            var contactResult = contactResponse.Resource;
+
+            contactResult.Id = contactTable.Id;
+            contactResult.ContactName = contactTable.ContactName;
+            contactResult.ContactType = contactTable.ContactType;
+            contactResult.Email = contactTable.Email;
+
+            contactResponse = await _container.ReplaceItemAsync<ContactTable>(contactResult, contactResult.Id);
+
+            if (contactResponse.Resource != null)
+            {
+              return contactResponse;
+            }
+            return null;
+        }
+
+        public async Task DeleteAsync(string id, string contactName)
+        {
+          ItemResponse<ContactTable> contactTable = await _container.DeleteItemAsync<ContactTable>(id, new PartitionKey(contactName));
+        }
     }
-  }
 }
